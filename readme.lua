ab896e4e0be6cd01c67edb0d78efeb99f199678f
DirkDigglerz <77217316+DirkDigglerz@users.noreply.github.com>
2022-09-09 18:22:43 +0100

    Update and rename readme.md to readme.lua

---
+++ readme.lua
@@ -0,0 +1,4 @@
+
+USAGE:
+
+exports['dirk-quizzes']:NewQuiz(Config.BountyHunterLicense.Title, Config.BountyHunterLicense.Questions, Config.BountyHunterLicense.RequiredToPass, "server", "Bounties:TestResult", {site = data.site})
