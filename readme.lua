d30abfa7cb86b98297b69deb18b30c31273929e1
DirkDigglerz <77217316+DirkDigglerz@users.noreply.github.com>
2022-09-09 18:24:45 +0100

    Update readme.lua

--- readme.lua
+++ readme.lua
@@ -2,8 +2,19 @@
 USAGE:
 
 local Questions = {
-
-}
+  ["How should you bring in a suspect?"] = {
+    ['Quietly and Alive'] = true, 
+    ['Loud and Fast']     = false, 
+    ['Quickly']           = false, 
+    ['Dead and Buried']   = false, 
+  },
+  ["How should you bring in a suspect? 2"] = {
+    ['Quietly and Alive'] = true, 
+    ['Loud and Fast']     = false, 
+    ['Quickly']           = false, 
+    ['Dead and Buried']   = false, 
+  },
+},
 
 exports['dirk-quizzes']:NewQuiz(title,questions,percentagetopass,eventtype, event, args)
 exports['dirk-quizzes']:NewQuiz('TITLE OF QUIZ', Questions, 25, "server", "Bounties:TestResult", {arg1 = "Arg21"})
